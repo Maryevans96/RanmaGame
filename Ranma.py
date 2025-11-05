4df044412611332ef35219f734c396a0cb429785
Maryevans96 <toppimariagrazia@yahoo.it>
2025-11-05 08:23:43 +0100

    inizio

---
+++ Ranma.py
@@ -0,0 +1,19 @@
+import pygame
+import random
+
+pygame.init()
+
+sfondo = pygame.image.load ('immagini/sfondo.jpg')
+ranmamale = pygame.image.load ('immagini/ranmamale.png')
+ranmafem = pygame.image.load ('immagini/ranmafem.png')
+akane = pygame.image.load ('immagini/akane.png')
+genma = pygame.image.load ('immagini/genma.png')
+happosai = pygame.image.load ('immagini/happosai.png')
+kuno = pygame.image.load ('immagini/kuno.png')
+acquaalta = pygame.image.load ('immagini/acquaalta.png')
+acquabassa = pygame.image.load ('immagini/acquabassa.png')
+teiera = pygame.image.load ('immagini/teiera.png')
+win = pygame.image.load('immagini/win.jpg')
+loser = pygame.image.load('immagini/loser.jpg')
+
+SCHERMO = pygame.display.set_mode((800, 600))
