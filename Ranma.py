31cb3679d03f7d581fcbc55605ef2a1138933b15
Maryevans96 <toppimariagrazia@yahoo.it>
2025-11-16 11:00:07 +0100

    funzione trasformazione e collisione acqua

--- Ranma.py
+++ Ranma.py
@@ -17,34 +17,72 @@ teiera = pygame.image.load ('immagini/teiera.png')
 win = pygame.image.load('immagini/win.jpg')
 loser = pygame.image.load('immagini/loser.jpg')
 
+ranmacorrente = ranmamale
+isranmamale =True
+
 SCHERMO = pygame.display.set_mode((960, 720))
 FPS = 50
 VEL_AVANZ =3
 
 class ostacoli_classe:
     def __init__(self):
-        self.x = 500
-        self.y = 440
+        self.x = random.randint(200,500)
+        self.y_genma = 440
+        self.y_happosai = 0
+        self.y_acquaalta = 0
+        self.y_acquabassa =440
+        self.y_teiera = random.randint(0,400)
+        self.tipo_ostacolo = random.choice(["genma", "happosai", "acquaalta","acquabassa"])
     def avanza_e_disegna (self):
         self.x -= VEL_AVANZ
-        SCHERMO.blit(genma,(self.x,self.y))
-        SCHERMO.blit(happosai, (self.x+200, self.y-440))
-    def collisione(self,ranmamale, ranmamalex, ranmamaley):
-        tolleranza = 3
+        if self.tipo_ostacolo == "genma":
+           SCHERMO.blit(genma,(self.x,self.y_genma))
+        elif self.tipo_ostacolo == "acquaalta":
+            SCHERMO.blit(acquaalta,(self.x,self.y_acquaalta))
+        elif self.tipo_ostacolo == "acquabassa":
+            SCHERMO.blit(acquabassa, (self.x, self.y_acquabassa))
+        elif self.tipo_ostacolo == "happosai":
+           SCHERMO.blit(happosai, (self.x, self.y_happosai))
+ def collisione(self,ranmamale, ranmamalex, ranmamaley):
+         tolleranza_hitbox = 20 # Prova valori più alti (es. 10, 15, 20)
 
-        ranmamale_lato_dx = ranmamalex+ranmamale.get_width ()-tolleranza
-        ranmamale_lato_sx = ranmamalex + tolleranza
-        genma_lato_dx = self.x + genma.get_width ()
-        genma_lato_sx = self.x
 
-        ranmamale_lato_su = ranmamaley+tolleranza
-        ranmamale_lato_giu= ranmamaley+ranmamale.get_height ()-tolleranza
-        genma_lato_su = self.y
-        genma_lato_giu = self.y+ genma.get_height()
-        if ranmamale_lato_dx>genma_lato_sx and ranmamale_lato_sx<genma_lato_dx:
-            if ranmamale_lato_su<genma_lato_giu and ranmamale_lato_giu > genma_lato_su:
-                hai_perso()
+        if self.tipo_ostacolo == "genma":
+            ostacolo_img = genma
+            ostacolo_y = self.y_genma
+        elif self.tipo_ostacolo == "acquabassa":
+            ostacolo_img = acquabassa
+            ostacolo_y = self.y_acquabassa
+        elif self.tipo_ostacolo == "happosai":
+            ostacolo_img = happosai
+            ostacolo_y = self.y_happosai
+        elif self.tipo_ostacolo == "acquaalta":
+            ostacolo_img = acquaalta
+            ostacolo_y = self.y_acquaalta
+        else:
+            return
 
+        ranmamale_lato_dx = ranmamalex + ranmamale.get_width() - tolleranza_hitbox  # Riduce a destra
+        ranmamale_lato_sx = ranmamalex + tolleranza_hitbox
+
+        # Usa l'immagine e le coordinate corrette
+        ostacolo_lato_dx = self.x + ostacolo_img.get_width() - tolleranza_hitbox
+        ostacolo_lato_sx = self.x + tolleranza_hitbox
+
+        ranmamale_lato_su = ranmamaley + tolleranza_hitbox # Riduce in alto
+        ranmamale_lato_giu = ranmamaley + ranmamale.get_height() # Riduce leggermente o lo lascia al piede
+
+        # Usa l'immagine e le coordinate corrette
+        ostacolo_lato_su = ostacolo_y + tolleranza_hitbox
+        ostacolo_lato_giu = ostacolo_y + ostacolo_img.get_height() - tolleranza_hitbox
+
+        # Controllo della collisione
+        if ranmamale_lato_dx > ostacolo_lato_sx and ranmamale_lato_sx < ostacolo_lato_dx:
+            if ranmamale_lato_su < ostacolo_lato_giu and ranmamale_lato_giu > ostacolo_lato_su:
+                if self.tipo_ostacolo == "genma" or self.tipo_ostacolo =="happosai":
+                    hai_perso()
+                elif self.tipo_ostacolo == "acquaalta" or self.tipo_ostacolo == "acquabassa":
+                    trasformazione()
 
 
 def aggiorna():
@@ -60,7 +98,6 @@ def inizializza():
     basex=0
     ostacoli= []
     ostacoli.append(ostacoli_classe())
-
 
 def disegna_oggetti():
     SCHERMO.blit(sfondo, (0,0))
@@ -84,7 +121,18 @@ def hai_perso():
             if event.type == pygame.QUIT:
                 pygame.quit()
 
-#inizializzo le variabili
+def trasformazione():
+    global ranmacorrente, isranmamale
+    if isranmamale:
+        ranmacorrente=ranmafem
+        isranmamale = False
+    elif not isranmamale:
+        ranmacorrente =ranmamale
+        isranmamale =True
+
+
+
+
 inizializza()
 
 #ciclo principale
